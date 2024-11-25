eb523148a426d3d0e325f9ac59804efedfa4ce6c
MohamadObeid9 <mohamad.obeid.2@isae.edu.lb>
2024-11-25 10:40:19 +0200

    the game can finally work properly in the console

--- script.js
+++ script.js
@@ -1,149 +1,125 @@
-/*
-** The Gameboard represents the state of the board
-** Each equare holds a Cell (defined later)
-** and we expose a dropToken method to be able to add Cells to squares
-*/
-
 function Gameboard() {
-    const rows = 3;
-    const columns = 3;
-    const board = [];
+  const rows = 3;
+  const columns = 3;
+  const board = [];
 
-    // Create a 2d array that will represent the state of the game board
-    // For this 2d array, row 0 will represent the top row and
-    // column 0 will represent the left-most column.
-    // This nested-loop technique is a simple and common way to create a 2d array.
-    for (let i = 0; i < rows; i++) {
-      board[i] = [];
-      for (let j = 0; j < columns; j++) {
-        board[i].push(Cell());
-      }
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
     }
-
-    // This will be the method of getting the entire board that our
-    // UI will eventually need to render it.
-    const getBoard = () => board;
-
-    // In order to drop a token, we need to find what the lowest point of the
-    // selected column is, *then* change that cell's value to the player number
-
-    const dropToken = (column, player) => {
-
-      // Our board's outermost array represents the row,
-      // so we need to loop through the rows, starting at row 0,
-      // find all the rows that don't have a token, then take the
-      // last one, which will represent the bottom-most empty cell
-      const availableCells = board.filter((row) => row[column].getValue() === 0).map(row =>row[column]);
-
-      // If no cells make it through the filter,
-      // the move is invalid. Stop execution.
-      if (!availableCells.length) return;
-
-      // Otherwise, I have a valid cell, the last one in the filtered array
-      const lowestRow = availableCells.length - 1;
-      board[lowestRow][column].addToken(player);
-
-    };
-
-    // This method will be used to print our board to the console.
-    // It is helpful to see what the board looks like after each turn as we play,
-    // but we won't need it after we build our UI
-    const printBoard = () => {
-      const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
-      console.table(boardWithCellValues);
-    };
-
-    // Here, we provide an interface for the rest of our
-    // application to interact with the board
-    return { getBoard, dropToken, printBoard };
   }
 
-  /*
-  ** A Cell represents one "square" on the board and can have one of
-  ** 0: no token is in the square,
-  ** 1: Player One's token,
-  ** 2: Player 2's token
-  */
+  const getBoard = () => board;
 
-  function Cell() {
-    let value = 0;
+  const dropToken = (row, column, player) => {
+    const selectedCell = board[row][column];
+    if (selectedCell.getValue() !== 0) return;
+    selectedCell.addToken(player);
+  };
 
-    // Accept a player's token to change the value of the cell
-    const addToken = (player) => {
-      value = player;
-    };
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+    console.table(boardWithCellValues);
+  };
 
-    // How we will retrieve the current value of this cell through closure
-    const getValue = () => value;
+  return { getBoard, dropToken, printBoard };
+}
 
-    return {
-      addToken,
-      getValue
-    };
-  }
+function Cell() {
+  let value = 0;
 
-  /*
-  ** The GameController will be responsible for controlling the
-  ** flow and state of the game's turns, as well as whether
-  ** anybody has won the game
-  */
-  function GameController(
-    playerOneName = "Player One",
-    playerTwoName = "Player Two"
-  ) {
-    const board = Gameboard();
+  const addToken = (player) => {
+    value = player;
+  };
 
-    const players = [
-      {
-        name: playerOneName,
-        token: "X"
-      },
-      {
-        name: playerTwoName,
-        token: "O"
-      }
-    ];
+  const getValue = () => value;
 
-    let activePlayer = players[0];
+  return {
+    addToken,
+    getValue,
+  };
+}
 
-    const switchPlayerTurn = () => {
-      activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    };
-    const getActivePlayer = () => activePlayer;
+function GameController(playerOneName = "X", playerTwoName = "O") {
+  const board = Gameboard();
 
-    const printNewRound = () => {
-      board.printBoard();
-      console.log(`${getActivePlayer().name}'s turn.`);
-    };
+  const players = [
+    {
+      name: playerOneName,
+      token: "X",
+    },
+    {
+      name: playerTwoName,
+      token: "O",
+    },
+  ];
 
-    const checkWin = (row,column) =>{
+  let activePlayer = players[0];
 
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const checkWin = () => {
+    const checkBoard = board.getBoard();
+    for (let i = 0; i < 3; i++) {
+      if (
+        checkBoard[i][0].getValue() === checkBoard[i][1].getValue() &&
+        checkBoard[i][1].getValue() === checkBoard[i][2].getValue() &&
+        checkBoard[i][2].getValue() !== 0
+      )
+        return true;
+      if (
+        checkBoard[0][i].getValue() === checkBoard[1][i].getValue() &&
+        checkBoard[1][i].getValue() === checkBoard[2][i].getValue() &&
+        checkBoard[2][i].getValue() !== 0
+      )
+        return true;
     }
-    const playRound = (column) => {
-      // Drop a token for the current player
-      console.log(
-        `Dropping ${getActivePlayer().name}'s token into column ${column}...`
-      );
-      board.dropToken(column, getActivePlayer().token);
+    if (
+      ((checkBoard[0][0].getValue() === checkBoard[1][1].getValue() &&
+        checkBoard[1][1].getValue() === checkBoard[2][2].getValue()) ||
+        (checkBoard[0][2].getValue() === checkBoard[1][1].getValue() &&
+          checkBoard[1][1].getValue() === checkBoard[2][0].getValue())) &&
+      checkBoard[1][1].getValue() !== 0
+    )
+      return true;
+    return false;
+  };
 
-      /*  This is where we would check for a winner and handle that logic,
-          such as a win message. */
-    checkWin();
+  const playRound = (row, column) => {
+    console.log(
+      `Dropping ${
+        getActivePlayer().name
+      }'s token into the cell [${row}][${column}]...`
+    );
+    board.dropToken(row, column, getActivePlayer().token);
 
-      // Switch player turn
+    if (checkWin()) {
+      printNewRound();
+      console.log(`${getActivePlayer().name} WINS!`);
+      console.log("please reload the screen to play again ")
+    } else {
       switchPlayerTurn();
       printNewRound();
-    };
+    }
+  };
 
-    // Initial play game message
-    printNewRound();
+  printNewRound();
 
-    // For the console version, we will only use playRound, but we will need
-    // getActivePlayer for the UI version, so I'm revealing it now
-    return {
-      playRound,
-      getActivePlayer
-    };
-  }
+  return {
+    playRound,
+    getActivePlayer,
+  };
+}
 
-  const game = GameController();
+const game = GameController();
