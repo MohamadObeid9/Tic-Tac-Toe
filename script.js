14407a8ee113f64dcf3dbd4de6e8951a0fd84a51
MohamadObeid9 <mohamad.obeid.2@isae.edu.lb>
2024-11-25 21:06:17 +0200

    the game now work properly in the browser

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function Gameboard() {
 
   const dropToken = (row, column, player) => {
     const selectedCell = board[row][column];
-    if (selectedCell.getValue() !== 0) return;
+    if (selectedCell.getValue() !== "") return;
     selectedCell.addToken(player);
   };
 
@@ -29,7 +29,7 @@ function Gameboard() {
 }
 
 function Cell() {
-  let value = 0;
+  let value = "";
 
   const addToken = (player) => {
     value = player;
@@ -75,13 +75,13 @@ function GameController(playerOneName = "X", playerTwoName = "O") {
       if (
         checkBoard[i][0].getValue() === checkBoard[i][1].getValue() &&
         checkBoard[i][1].getValue() === checkBoard[i][2].getValue() &&
-        checkBoard[i][2].getValue() !== 0
+        checkBoard[i][2].getValue() !== ""
       )
         return true;
       if (
         checkBoard[0][i].getValue() === checkBoard[1][i].getValue() &&
         checkBoard[1][i].getValue() === checkBoard[2][i].getValue() &&
-        checkBoard[2][i].getValue() !== 0
+        checkBoard[2][i].getValue() !== ""
       )
         return true;
     }
@@ -90,7 +90,7 @@ function GameController(playerOneName = "X", playerTwoName = "O") {
         checkBoard[1][1].getValue() === checkBoard[2][2].getValue()) ||
         (checkBoard[0][2].getValue() === checkBoard[1][1].getValue() &&
           checkBoard[1][1].getValue() === checkBoard[2][0].getValue())) &&
-      checkBoard[1][1].getValue() !== 0
+      checkBoard[1][1].getValue() !== ""
     )
       return true;
     return false;
@@ -107,7 +107,9 @@ function GameController(playerOneName = "X", playerTwoName = "O") {
     if (checkWin()) {
       printNewRound();
       console.log(`${getActivePlayer().name} WINS!`);
-      console.log("please reload the screen to play again ")
+      console.log("please reload the screen to play again ");
+      alert(`${activePlayer.name} is the WINNER!!`);
+      location.reload();//reload the screen after announcement the winner
     } else {
       switchPlayerTurn();
       printNewRound();
@@ -119,7 +121,59 @@ function GameController(playerOneName = "X", playerTwoName = "O") {
   return {
     playRound,
     getActivePlayer,
+    getBoard: board.getBoard,
+    checkWin,
   };
 }
 
-const game = GameController();
+function ScreenController() {
+  const game = GameController();
+  const playerTurnDiv = document.querySelector(".turn");
+  const boardDiv = document.querySelector(".board");
+
+  const updateScreen = () => {
+    // clear the board
+    boardDiv.textContent = "";
+
+    // get the newest version of the board and player turn
+    const board = game.getBoard();
+    const activePlayer = game.getActivePlayer();
+
+    // Display player's turn
+    playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
+
+    // Render board squares
+    board.forEach((row, rowIndex) => {
+      row.forEach((cell, columnIndex) => {
+        // Anything clickable should be a button!!
+        const cellButton = document.createElement("button");
+        cellButton.classList.add("cell");
+        // Create a data attribute to identify the column
+        // This makes it easier to pass into our `playRound` function
+        cellButton.dataset.column = columnIndex;
+        cellButton.dataset.row = rowIndex;
+        cellButton.textContent = cell.getValue();
+        boardDiv.appendChild(cellButton);
+      });
+    });
+  };
+
+  // Add event listener for the board
+  function clickHandlerBoard(e) {
+    const selectedColumn = e.target.dataset.column;
+    const selectedRow = e.target.dataset.row;
+    // Make sure I've clicked a column and not the gaps in between
+    if (!selectedColumn && !selectedRow) return;
+
+    game.playRound(selectedRow, selectedColumn);
+    updateScreen();
+  }
+  boardDiv.addEventListener("click", clickHandlerBoard);
+
+  // Initial render
+  updateScreen();
+
+  // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
+}
+
+ScreenController();
